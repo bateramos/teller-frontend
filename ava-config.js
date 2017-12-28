7c9687f87cd14fcfedcbab2833f3b4b915fe8ef8
Pedro Ferreira Ramos <piratamoendobateria@gmail.com>
2017-12-28 08:41:57 -0200

    add test functinalities

--- ava-config.js
+++ ava-config.js
@@ -1,4 +1,15 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
 Enzyme.configure({ adapter: new Adapter() });
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
--- src/components/nodes/TestNode.test.js
+++ src/components/nodes/TestNode.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import test from 'ava';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { spy } from 'sinon';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextNode from './TextNode';
 
 const defaultProps = {
@@ -12,7 +14,48 @@ const defaultProps = {
   onConnect: () => {},
 };
 
+function render(component) {
+  return mount(
+    <MuiThemeProvider>
+      {component}
+    </MuiThemeProvider>
+  );
+}
+
 test('renders without crashing', t => {
-  const wrapper = shallow(<TextNode {...defaultProps} />);
+  const wrapper = render(<TextNode {...defaultProps} />);
   t.pass();
+});
+
+test('render connection mode', t => {
+  const onConnect = spy();
+  const wrapper = render(<TextNode {...{...defaultProps, onConnect} } connectionMode={true}/>);
+  t.is(wrapper.find('RaisedButton').length, 1)
+  wrapper.find('RaisedButton').find('button').simulate('click');
+  t.true(onConnect.calledOnce);
+});
+
+test('render cancel connection mode', t => {
+  const onCreateConnection = spy();
+  const onCancelCreateConnect = spy();
+  const wrapper = render(<TextNode {...{...defaultProps, onCreateConnection, onCancelCreateConnect} } connectionMode={false} hasConnection={false}/>);
+  t.is(wrapper.find('RaisedButton').length, 1);
+  wrapper.find('RaisedButton').find('button').simulate('click');
+  t.true(onCreateConnection.calledOnce);
+  t.is(wrapper.find('RaisedButton').length, 1);
+  wrapper.find('RaisedButton').find('button').simulate('click');
+  t.true(onCancelCreateConnect.calledOnce);
+});
+
+test('render without connection in connection mode', t => {
+  const onCreateConnection = spy();
+  const wrapper = render(<TextNode {...{...defaultProps, onCreateConnection} } hasConnection={false}/>);
+  t.is(wrapper.find('RaisedButton').length, 1)
+  wrapper.find('RaisedButton').find('button').simulate('click');
+  t.true(onCreateConnection.calledOnce);
+});
+
+test('render connection mode when is already as connection', t => {
+  const wrapper = render(<TextNode {...defaultProps} hasConnection={true} connectionMode={true}/>);
+  t.is(wrapper.find('RaisedButton').length, 0)
 });
